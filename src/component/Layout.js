e1a55e9a4d15b86961e939c6a0529a379d797327
zakiamarrit <zakiamarrit@gmail.com>
2022-11-25 20:57:30 +0700

    update title

--- src/component/Layout.js
+++ src/component/Layout.js
@@ -1,10 +1,12 @@
-import "./Layout.css"
+import './Layout.css';
 
-export default function Layout({children}){
-    return <div className="main">
-        <div className="header">
-                <h1 className="header-brand">ZAKIATEL</h1>
-        </div>   
-        {children}
+export default function Layout({ children }) {
+  return (
+    <div className="main">
+      <div className="header">
+        <h1 className="header-brand">ZAKIA HOTEL</h1>
+      </div>
+      {children}
     </div>
+  );
 }
