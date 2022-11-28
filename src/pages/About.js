8b5fe5d62dd4d4e226470deeef9d8a33be770e2b
zakiamarrit <zakiamarrit@gmail.com>
2022-11-28 18:39:08 +0700

    update profil

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,7 +7,7 @@ export default function About() {
       <div className="contentAbout">
         <img src="/images/zakia hotel.png" className="contentAboutImg" alt="rocket"></img>
         <h2 className="contentAboutTitle">Tentang zakia hotel</h2>
-        <p className="contentAboutBody">Aplikasi ini dikembangkan oleh Zakia Marrit Mahasiswa Universitas Diponegoro Jurusan Teknik Komputer Angkatan 2020, dengan tujuan memenuhi tugas akhir praktikum pemrograman perangkat bergerak</p>
+        <p className="contentAboutBody">Aplikasi buatan Zakia.</p>
       </div>
     </Layout>
   );
